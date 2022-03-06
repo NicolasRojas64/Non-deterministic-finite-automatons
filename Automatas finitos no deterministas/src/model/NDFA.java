fc95d709720978519cae3fc08dbaeb8955e0b8b4
NicolasRojas64 <91982000+NicolasRojas64@users.noreply.github.com>
2022-03-06 16:14:33 -0500

    Add files via upload

---
+++ Automatas finitos no deterministas/src/model/NDFA.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.ArrayList;
+
+public class NDFA {
+	private ArrayList<State> states;
+	private ArrayList<Transition> transitions;
+	
+	public NDFA() {
+		
+	}
+	
+	public void addState(State state) {
+		
+	}
+	
+	public void deleteState(State state) {
+		
+	}
+	
+	public void addTransition(char transitionValue, State initialState, State finalState) {
+		
+	}
+	
+	public void deleteTransition(Transition transition) {
+		
+	}
+	
+	
+}
